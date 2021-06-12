202a32d1ee1608574cff38fc712dcab5f0dce71c
longvb <vuongbaolong48@gmail.com>
2021-06-12 10:00:47 +0700

    init project

---
+++ src/main/java/org/vnu/unname/UnnameApplication.java
@@ -0,0 +1,13 @@
+package org.vnu.unname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnnameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnnameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vnu/unname/UnnameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vnu.unname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnnameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
